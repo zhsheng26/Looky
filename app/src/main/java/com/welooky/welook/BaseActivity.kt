93bd9513398e4a408da79d0673fe69fa44dddacd
zhangsheng <zhsheng26@gmail.com>
2019-05-07 23:12:29 +0800

    init

---
+++ app/src/main/java/com/welooky/welook/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.welooky.welook
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_base.*
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
